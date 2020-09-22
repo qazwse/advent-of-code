c391bd74ade42bf20a4f58c4f11129e40752038f
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-09-22 15:16:07 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject advent-of-code "0.1.0-SNAPSHOT"
+  :description "Advent of Code 2019 - Clojure Style"
+  :url "http://curtisrasmussen.ca"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns advent-of-code.core}
+  :resource-paths ["resources"])
