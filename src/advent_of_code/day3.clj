27827c07cd686cbe4ede5695239337e9ea8ab4f4
Curtis Rasmussen <crasmuss@alumni.uoguelph.ca>
2020-09-22 21:05:31 -0400

    Read input, start finding intersections... (not fun)...

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -5,6 +5,28 @@
 ;; Calculate Manhattan Distance - point closest to origin where two lines intersect
 ;; Input given as D####, where D can be U[p] D[own] L[eft] R[ight]
 
+(defn split-line-segments
+  "Given a list of line segments, will return them seperated into horizontal and vertical groups."
+  [line-segments]
+  [])
+
+(defn calc-intersection
+  "calculates intersection"
+  [[a1 a2] [b1 b2]]
+  )
+
+(defn find-intersections
+  "finds intersections between two wires. Our wires have straight lines,
+  so we can only intersect horizontal to vertical. If we sorted our the
+  line segments it may cut down on complexity too."
+  [wire-a wire-b]
+  (let [intersections #{}]
+    (loop [seg-a (take 2 wire-a)
+           rem-a (rest wire-a)]
+      (loop [seg-b (take 2 wire-b)
+             rem-b (take 2 (wire-b))]
+        ))))
+
 (defn get-point
   "creates new point from direction code and previous point"
   [code [x y]]
@@ -33,4 +55,4 @@
        (map #(str/split % #","))
        (map to-instructions)))
 
-(get-input "day3-input")
+(def test (first (get-input "day3-input")))
